8a8fe29246f51a0819d9f2cb1108aacf045cfb6e
Eledhrim <Eledhrim@live.com>
2019-12-23 23:43:49 +0300

    2nd

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public BooksController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View(_dbContext.Books.ToList());
+        }
+
+        //GET Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(book);
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        //GET Book/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _dbContext.Books.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                //_dbContext.Update(book);
+
+                var BookFromDb = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == book.Id);
+                BookFromDb.Name = book.Name;
+                BookFromDb.Author = book.Author;
+                BookFromDb.Price = book.Price;
+
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
+        }
+
+        //GET Book/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var book = await _dbContext.Books.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveBook(int? id)
+        {
+
+
+            var book = await _dbContext.Books.FirstOrDefaultAsync(b => b.Id == id);
+            _dbContext.Books.Remove(book);
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+
+
+}
